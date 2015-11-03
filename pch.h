7dd8638f9e7b94fd91e2604394189dd4a5c8592e
mnogono <mnogono@gmail.com>
2015-11-03 15:55:19 +0500

    update sysFile::GetFileLastModifyDateTimeUTC function

---
+++ pch.h
@@ -0,0 +1,62 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "windows.h"
+
+//standard borland headers --------------------------
+#include <vcl.h>
+#include <Data.DBXJSON.hpp>
+#include <Vcl.Grids.hpp>
+#include <System.DateUtils.hpp>
+
+//standard headers ----------------------------------
+#include <assert.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <float.h>
+#include <math.h>
+#include <TlHelp32.h>
+#include <cstdio>
+#include <float.h>
+
+//standard STL headers ------------------------------
+#include <string>
+#include <vector>
+#include <map>
+#include <deque>
+#include <list>
+#include <set>
+#include <exception>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <valarray>
+
+//external sys headers ------------------------------
+#include "crc.h"
+#include "sysSystem.h"
+#include "sysBaseTypes.h"
+#include "sysException.h"
+#include "sysFile.h"
+#include "sysStr.h"
+#include "sysJSON.h"
+#include "sysTime.h"
+#include "sysKey.h"
+#include "sysMath.h"
+#include "sysStr.h"
+#include "sysObserver.h"
+#include "sysObserverable.h"
+#include "sysPatterns.h"
+#include "sysProcess.h"
+#include "sysReg.h"
+#include "sysThread.h"
+#include "sysLogger.h"
+#include "sysControlState.h"
+#include "sysApp.h"
+#include "sysOO.h"
+#include "sysVCL.h"
+
+#endif
